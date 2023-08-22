244d2850e43938dd8c1ff5d24b88f18f8e2b5b20
slogup-woodrow <116634186+slogup-woodrow@users.noreply.github.com>
2023-08-22 18:55:42 +0900

    fix:re-

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-red-500">
+      <div className="text-blue-200 text-center">123</div>
+    </div>
+  );
+};
+
+export default Home;
